4cf839a8466ad931a1e65d761b5affd4449af15a
Wietse Wind <mail@wietse.com>
2019-09-04 12:25:53 +0200

    Sep. WS from admin server

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ if (typeof process.env.JEST_WORKER_ID === 'undefined') {
 }
 
 const log = Debug('app')
+
+/**
+ * WS server
+ */
 const app = express()
 app.use(helmet({
   frameguard: {
@@ -20,30 +24,48 @@ app.use(helmet({
   }
 }))
 app.use(cors())
+
 const server = http.createServer(app)
 const wss = new WebSocket.Server({server})
 
 server.listen(process.env.PORT || 80, () => {
   const address = server.address()
-  const port = typeof address !== 'string'
-    ? address!.port || -1
-    : -1
-
-  log(`Server started at port ${port} :)`)
+  const port = typeof address !== 'string' ? address!.port || -1 : -1
+  log(`WS server started at port ${port}`)
 })
 
+/**
+ * Admin server
+ */
+
+const adminApp = express()
+const adminServer = http.createServer(adminApp)
+adminServer.listen(Number(process.env.PORT || 80) + 1, () => {
+  const address = adminServer.address()
+  const port = typeof address !== 'string' ? address!.port || -1 : -1
+  log(`ADMIN server started at port ${port}`)
+})
+
+/**
+ * Run proxy and Admin server
+ */
+
 const proxy = new ProxyServer(wss)
-const httpserver = new HttpServer(app, proxy)
+const admin = new HttpServer(adminApp, proxy)
+
+/**
+ * Remaining stuff
+ */
 
 const shutdown = async () => {
   return new Promise((resolve, reject) => {
-    server.on('close', () => {
-      resolve()
-    })
-    server.on('error', e => {
-      reject(e)
-    })
+    server.on('close', () => resolve())
+    server.on('error', e => reject(e))
     server.close()
+
+    adminServer.on('close', () => resolve())
+    adminServer.on('error', e => reject(e))
+    adminServer.close()
   })
 }
 
